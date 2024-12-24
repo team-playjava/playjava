e47cabdf4ab9579df10bf0241cfef08cb1e1b4c1
Dongwan Seo <kr.ihah@kakao.com>
2024-12-25 03:47:43 +0900

    chore: discord role add in linking sorryfield account

--- src/app/api/user/link/route.ts
+++ src/app/api/user/link/route.ts
@@ -1,11 +1,16 @@
-import { PrismaClient, Song } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v10'
 
 const prisma = new PrismaClient()
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN as string);
+
 export async function POST(
 	req: NextRequest
 ) {
-	const data = await req.json()
+	const { data } = await req.json()
 
 	try {
 		const user = await prisma.user.update({
@@ -15,6 +20,12 @@ export async function POST(
 			}
 		});
 
+		const account = await prisma.account.findFirst({
+			where: { userId: user.id }
+		})
+
+		await rest.put(Routes.guildMemberRole('1162778955884011663', account?.providerAccountId as string, '1297480308614103070'))
+		
 		return new NextResponse(JSON.stringify(
 			{ user: user }
 		), {
